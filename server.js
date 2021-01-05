f96ae360ef75e3f6b940b4539a6cd6c9ac07ec9c
yongmun <deathray444@naver.com>
2021-01-05 18:09:33 +0900

    Create Customer Database Table & Node.js Database Connection Settings

--- server.js
+++ server.js
@@ -2,37 +2,30 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
 const port = process.env.PORT || 5000;
+const mysql = require("mysql"); // << 새로 추가된 부분
+const fs = require("fs");
+
+const data = fs.readFileSync("./database.json");
+const conf = JSON.parse(data);
+
+const connection = mysql.createConnection({
+  /// 새로 추가된 부분
+  host: conf.host,
+  user: conf.user, // mysql에 아이디를 넣는다.
+  password: conf.password, // mysql의 비밀번호를 넣는다.
+  port: conf.port,
+  database: conf.database, //위에서 만든 데이터베이스의 이름을 넣는다.
+});
+
+connection.connect();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/api/customers", (req, res) => {
-  res.send([
-    {
-      id: 1,
-      image: "http://placeimg.com/64/64/1",
-      name: "홍길동",
-      birthday: "961222",
-      gender: "남자",
-      job: "대학생",
-    },
-    {
-      id: 2,
-      image: "http://placeimg.com/64/64/2",
-      name: "문자두",
-      birthday: "961222",
-      gender: "여자",
-      job: "대학생",
-    },
-    {
-      id: 3,
-      image: "http://placeimg.com/64/64/3",
-      name: "문 용",
-      birthday: "961222",
-      gender: "남자",
-      job: "프로게이머",
-    },
-  ]);
+  connection.query("SELECT * FROM CUSTOMER", (err, rows, fields) => {
+    res.send(rows);
+  });
 });
 
 app.listen(port, () => console.log(`Listening on port${port}`));
